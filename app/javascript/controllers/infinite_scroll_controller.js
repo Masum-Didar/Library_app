1f843af640b022e9481122c4fb73992073ed1546
Abu Masum Didar <masumdidar.cse@gmail.com>
2024-07-11 19:47:40 +0600

    now add infinite scrolling

--- app/javascript/controllers/infinite_scroll_controller.js
+++ app/javascript/controllers/infinite_scroll_controller.js
@@ -28,8 +28,15 @@ export default class extends Controller {
     }
 
     loadMore() {
-        const nextPage = this.data.get("next-page")
+        let nextPage = this.data.get("next-page")
         console.log(nextPage)
+
+        // If nextPage is null, load the first page
+        if (!nextPage) {
+            nextPage = this.data.get("first-page")
+            this.data.set("next-page", nextPage)
+        }
+
         if (nextPage) {
             fetch(nextPage, {
                 headers: { Accept: "text/vnd.turbo-stream.html" }
@@ -51,4 +58,30 @@ export default class extends Controller {
             this.data.delete("next-page")
         }
     }
+
+
+    // loadMore() {
+    //     const nextPage = this.data.get("next-page")
+    //     console.log(nextPage)
+    //     if (nextPage) {
+    //         fetch(nextPage, {
+    //             headers: { Accept: "text/vnd.turbo-stream.html" }
+    //         })
+    //             .then(response => response.text())
+    //             .then(html => {
+    //                 Turbo.renderStreamMessage(html)
+    //                 this.updateNextPage()
+    //             })
+    //     }
+    // }
+    //
+    // updateNextPage() {
+    //     const nextPageLink = document.querySelector("[data-next-page-link]")
+    //     if (nextPageLink) {
+    //         this.data.set("next-page", nextPageLink.getAttribute("href"))
+    //         nextPageLink.remove()
+    //     } else {
+    //         this.data.delete("next-page")
+    //     }
+    // }
 }
