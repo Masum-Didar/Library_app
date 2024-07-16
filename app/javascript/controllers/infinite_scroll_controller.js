78f92203cb50e4070e7fa9e325ab03b799ff9f33
Abu Masum Didar <masumdidar.cse@gmail.com>
2024-07-16 12:33:58 +0600

    update index and infinite scroll

--- app/javascript/controllers/infinite_scroll_controller.js
+++ app/javascript/controllers/infinite_scroll_controller.js
@@ -4,26 +4,19 @@ export default class extends Controller {
     static targets = ["anchor"]
 
     connect() {
-        this.observer = new IntersectionObserver(entries => {
-            entries.forEach(entry => {
-                if (entry.isIntersecting) {
-                    this.loadMore()
-                }
-            })
-        })
-
         this.observe()
     }
 
     observe() {
         if (this.hasAnchorTarget) {
+            this.observer = new IntersectionObserver(entries => {
+                entries.forEach(entry => {
+                    if (entry.isIntersecting) {
+                        this.loadMore()
+                    }
+                })
+            })
             this.observer.observe(this.anchorTarget)
-        }
-    }
-
-    disconnect() {
-        if (this.hasAnchorTarget) {
-            this.observer.unobserve(this.anchorTarget)
         }
     }
 
@@ -58,30 +51,4 @@ export default class extends Controller {
             this.data.delete("next-page")
         }
     }
-
-
-    // loadMore() {
-    //     const nextPage = this.data.get("next-page")
-    //     console.log(nextPage)
-    //     if (nextPage) {
-    //         fetch(nextPage, {
-    //             headers: { Accept: "text/vnd.turbo-stream.html" }
-    //         })
-    //             .then(response => response.text())
-    //             .then(html => {
-    //                 Turbo.renderStreamMessage(html)
-    //                 this.updateNextPage()
-    //             })
-    //     }
-    // }
-    //
-    // updateNextPage() {
-    //     const nextPageLink = document.querySelector("[data-next-page-link]")
-    //     if (nextPageLink) {
-    //         this.data.set("next-page", nextPageLink.getAttribute("href"))
-    //         nextPageLink.remove()
-    //     } else {
-    //         this.data.delete("next-page")
-    //     }
-    // }
 }
